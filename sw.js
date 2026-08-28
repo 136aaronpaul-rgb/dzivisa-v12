56920aeda929d2168ec155ebb6d81c366285ff5f
Aaron Paul <136aaronpaul@gmail.com>
2026-08-28 10:07:04 +0200

    FINAL COMMIT V25.2 AQUA 1787903133 - LIVE dzivisa-v12.surge.sh - ALL TOF INSIDE

--- sw.js
+++ sw.js
@@ -1,33 +1,22 @@
-const CACHE_NAME = 'dzivisa-v25-2-aqua-working-1787902833';
+const CACHE_NAME = 'dzivisa-v25-2-WORKING-FINAL-1787903133';
 const urlsToCache = [
   './',
-  './index.html',
-  './manifest.json',
+  './index.html?v=dzivisa-v25-2-WORKING-FINAL-1787903133',
+  './manifest.json?v=dzivisa-v25-2-WORKING-FINAL-1787903133',
   './icon-192.png',
   './icon-512.png',
-  './wallpaper.png',
-  './database.js',
-  './engine.js',
-  './runner.js',
-  './code-engine.js',
-  './override.js',
-  './langs/dictionary.js',
-  './plugins/loader.js',
-  './plugins/power-ui.js',
-  './plugins/reporter.js',
-  './plugins/viral.js'
-]
-
+  './database.js?v=dzivisa-v25-2-WORKING-FINAL-1787903133',
+  './engine.js?v=dzivisa-v25-2-WORKING-FINAL-1787903133',
+  './override.js?v=dzivisa-v25-2-WORKING-FINAL-1787903133'
+];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request, {cache: 'no-store'})));
 });
